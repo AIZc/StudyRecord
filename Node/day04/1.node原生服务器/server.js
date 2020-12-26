c00afe465b192862c3e5d6b2d203356f1af0075b
AIZc <583479637@qq.com>
2020-12-26 19:19:43 +0800

    2020-12-25 node express 学习 提交.

--- Node/day04/1.node原生服务器/server.js
+++ Node/day04/1.node原生服务器/server.js
@@ -4,11 +4,17 @@
 
 //引入node内置的http模块
 let http = require('http');
+// 引入一个内部模块,用于解析url
+let qs = require('querystring')
 
 // 1. 创建一个服务对象
 let server = http.createServer(function (request,response) {
+    let params = request.url.split('?')[1]
+    let objParams = qs.parse(params)
+    console.log(objParams)
+    let {name,age} = objParams;
     response.setHeader('content-type','text/html; charset=utf-8')
-    response.end('<h1>你好,我是你学习node的第一个服务器!</h1>');
+    response.end(`<h1>你好,我的名字是${name}!年龄${age}</h1>`);
 })
 // 2. 让服务对象干活,获取客户端的请求,联系数据库取数据
 
---
+++ Node/day04/3.express服务器/Study_express.js
@@ -0,0 +1,26 @@
+//引入express框架
+
+const express = require('express')
+
+const app = express()
+app.get('/',function (request, response) {
+    console.log(request.query)
+    response.send('我是主页!')
+})
+
+app.get('/ms',function (request, response) {
+    response.send('我是美食主页!')
+})
+
+app.get('/ms/c17',function (request, response) {
+    response.send('我是美食-火锅主页!')
+})
+
+app.post('/',function (request, response) {
+    response.send('post请求')
+})
+
+app.listen(8000,function (err) {
+    if (err) console.log(err);
+    else console.log('服务器启动成功!');
+})
